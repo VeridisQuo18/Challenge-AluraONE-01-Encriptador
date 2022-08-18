50ecc18637f28e7cdbed2627715e9919f9fb1656
Raxtion Music <raxtion.music@gmail.com>
2022-08-18 15:09:24 -0500

    finished program

--- main.js
+++ main.js
@@ -1,116 +1,164 @@
-
-var botonUno = document.querySelector("#btn1");
-var botonCopiar = document.querySelector("#btn-copy");
-
-var encriptacionExitosa = "TEXTO ENCRIPTADO:";
-var desencriptacionExitosa = "TEXTO DESENCRIPTADO:";
-
-
-var si = "";
+var msg;
+var msgEncrypt;
+var msgDecrypt;
+var btn1 = document.querySelector("#btn1");
+var btn2 = document.querySelector("#btn2");
+var btnCopy = document.querySelector("#btn-copy");
+var ESuccessful = "Encrypted!";
+var DSuccessful = "Decrypted!";
+var Mayus = /[A-Z]/g;
+var Char = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;´.,áéíóúàèìòù']/g;
+var yes = "";
 var no = "none";
 
-function resultado(ver){
-  document.getElementById("conteiner-resultado").style.display = ver;  
-  document.getElementById("btn-copy").style.display = ver;
-  document.getElementById("resultado").style.display = ver;
+// Acciones a botones
+btn1.onclick = Encrypt;
+btn2.onclick = Decrypt;
+btnCopy.onclick = ToCopy;
+
+// Pantalla resultados
+function ShowResult(ver){
+  document.getElementById("conteiner-result")
+          .style
+          .display = ver;  
+  document.getElementById("btn-copy")
+          .style
+          .display = ver;
+  document.getElementById("result")
+          .style
+          .display = ver;
 }
 
-document.getElementById("toy").style.display = "";
-
-resultado(no);
+document.getElementById("toy")
+        .style
+        .display = "";
+ShowResult(no);
 
 //Encriptar
-function encriptar(){
-  document.getElementById("toy").style.display = "none";
-  var msjEncriptado = "";
-  var mensaje = document.querySelector("#introducir-texto").value;
-  var mayus = /[A-Z]/g;
-  var caracteres = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/g;
-
-  if(mensaje == ""){
-    alert("no debe estar vacio");
-    document.getElementById("toy").style.display = "";
-    resultado(no)
-  }else if (mensaje.match(mayus) != mensaje.match(mayus)){
-      alert("No puede contener mayusculas");
-      document.getElementById("toy").style.display = "";
-  }else if (mensaje.match(caracteres) != mensaje.match(caracteres)){
-      alert("no puede contener acentos ni caracteres especiales");
-      document.getElementById("toy").style.display = "";
+function Encrypt(){
+  msgEncrypt = "";
+  document.getElementById("toy")
+          .style
+          .display = "none";
+  msg = document.querySelector("#inputText")
+                .value;
+  if(msg == ""){
+    show(0);
+    document.getElementById("toy")
+            .style
+            .display = "";
+    ShowResult(no);
+  }else if (msg.match(Mayus)){
+    show(1);
+    document.getElementById("toy")
+            .style
+            .display = "";
+  }else if (msg.match(Char)){
+    show(2);
+    document.getElementById("toy")
+            .style
+            .display = "";
   }else{
-    for(var i = 0; i < mensaje.length; i++){
-    if(mensaje[i] == "a"){
-      var encriptando = mensaje[i].replace("a","ai",);
-      msjEncriptado = msjEncriptado + encriptando;
-    }else if(mensaje[i] == "e"){
-      var encriptando = mensaje[i].replace("e","enter");
-      msjEncriptado = msjEncriptado + encriptando;
-    }else if(mensaje[i] == "i"){
-      var encriptando = mensaje[i].replace("i","imes");
-      msjEncriptado = msjEncriptado + encriptando;
-    }else if(mensaje[i] == "o"){
-      var encriptando = mensaje[i].replace("o","ober");
-      msjEncriptado = msjEncriptado + encriptando;
-    }else if(mensaje[i] == "u"){
-      var encriptando = mensaje[i].replace("u","ufat");
-      msjEncriptado = msjEncriptado + encriptando;
-  }else{
-      msjEncriptado = msjEncriptado + mensaje[i];
-    }
+    for(var i = 0; i < msg.length; i++){
+      if(msg[i] == "a"){
+        var ToEncrypt = msg[i]
+                        .replace("a", "159",);
+        msgEncrypt += ToEncrypt;
+      }else if(msg[i] == "e"){
+        var ToEncrypt = msg[i]
+                        .replace("e", "487");
+        msgEncrypt += ToEncrypt;
+      }else if(msg[i] == "i"){
+        var ToEncrypt = msg[i]
+                        .replace("i", "879");
+        msgEncrypt += ToEncrypt;
+      }else if(msg[i] == "o"){
+        var ToEncrypt = msg[i]
+                        .replace("o", "747");
+        msgEncrypt += ToEncrypt;
+      }else if(msg[i] == "u"){
+        var ToEncrypt = msg[i]
+                        .replace("u", "126");
+        msgEncrypt += ToEncrypt;
+      }else{
+        msgEncrypt += msg[i];
+      }
   }
-      document.getElementById("titulo-resultado").innerHTML=encriptacionExitosa;
-      document.getElementById("resultado").innerHTML=msjEncriptado;
-      document.getElementById("introducir-texto").value = "";
-      resultado(si)
+      document.getElementById("title-result")
+              .innerHTML=ESuccessful;
+      document.getElementById("result")
+              .innerHTML=msgEncrypt;
+      document.getElementById("inputText")
+              .value = "";
+      ShowResult(yes);
   }
 }
-
-botonUno.onclick = encriptar;
 
 // Copiar resultado
-function copiarPortapapeles(){
-
-  var copiado = document.getElementById("resultado");
-  copiado.select();
+function ToCopy(){
+  var copy = document.getElementById("result")
+                     .select();
   document.execCommand('copy');  
-
 }
-
-botonCopiar.onclick = copiarPortapapeles;
 
 //Desencriptar
-var botonDos = document.querySelector("#btn2");
-var mensaje = document.querySelector("#introducir-texto").value
-
-
-function desencriptar(){
-    document.getElementById("toy").style.display = "none";
-    var msjDesencriptado = "";
-    var mensaje = document.querySelector("#introducir-texto").value;
-    var mayus = /[A-Z]/g;
-    var caracteres = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/g;
-
-    if(mensaje == ""){
-        alert("Necesita ingresar texto");
-        resultado(no)
-        document.getElementById("toy").style.display = "";
-    }else if (mensaje.match(mayus)!= mensaje.match(mayus)){
-        alert("No se admiten mayusculas")
-        resultado(no)
-        document.getElementById("toy").style.display = "";
-    }else if (mensaje.match(caracteres) != mensaje.match(caracteres)){
-        alert("No se aceptan acentos y/o caracteres especiales")    
-        resultado(no);
-        document.getElementById("toy").style.display = "";
+function Decrypt(){
+  msgDecrypt = "";
+  document.getElementById("toy")
+          .style
+          .display = "none";
+  msg = document.querySelector("#inputText")
+                .value;
+  if(msg == ""){
+    show(0);
+    ShowResult(no);
+    document.getElementById("toy")
+            .style
+            .display = "";
+    }else if (msg.match(Mayus)){
+        show(1)
+        ShowResult(no);
+        document.getElementById("toy")
+                .style
+                .display = "";
+    }else if (msg.match(Char)){
+        show(2)
+        ShowResult(no);
+        document.getElementById("toy")
+                .style
+                .display = "";
     }else{
-    msjDesencriptado = mensaje.replace(/ai/g, "a").replace(/enter/g,"e").replace(/imes/g,"i").replace(/ober/g,"o").replace(/ufat/g,"u"); 
-    document.getElementById("resultado").innerHTML=msjDesencriptado;
-    document.getElementById("titulo-resultado").innerHTML=desencriptacionExitosa;
-  
-    resultado(si)
-    document.getElementById("introducir-texto").value = ""
+      msgDecrypt = msg.replace(/159/g, "a")
+                      .replace(/487/g,"e")
+                      .replace(/879/g,"i")
+                      .replace(/747/g,"o")
+                      .replace(/126/g,"u");
+
+      document.getElementById("result")
+              .innerHTML = msgDecrypt;
+      document.getElementById("title-result")
+              .innerHTML = DSuccessful;
+      ShowResult(yes);
+      document.getElementById("inputText")
+              .value = ""
     }
-    
 }
 
-  botonDos.onclick = desencriptar;
+function show(code){
+  switch (code) {
+    case 0:
+      alert("This operation cannot be performed without text.");
+      break;
+
+    case 1:
+      alert("This operation does not allow the use of capital letters. Try again.");
+      break;
+
+    case 2:
+      alert("This operation does not allow the use of accented vowels and/or special characters.");
+      break;
+  
+    default:
+      break;
+  }
+}
